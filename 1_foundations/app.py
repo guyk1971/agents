e182fd138e5db29f6d95a6d065d388d63661b213
guyk1971 <guy.koren@gmail.com>
2025-09-11 16:29:50 +0300

    wk1 last update in app.py

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -77,14 +77,19 @@ class Me:
 
     def __init__(self):
         self.openai = OpenAI()
-        self.name = "Ed Donner"
-        reader = PdfReader("me/linkedin.pdf")
+        self.model_name = "gpt-5-nano"
+        # self.gemini = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+        # self.model_name = "gemini-2.5-flash" 
+        # self.ollama = OpenAI(base_url='http://localhost:11434/v1', api_key='ollama')
+        # self.model_name = "gpt-oss:20b"
+        self.name = "Guy Koren"
+        reader = PdfReader("me/Profile.pdf")
         self.linkedin = ""
         for page in reader.pages:
             text = page.extract_text()
             if text:
                 self.linkedin += text
-        with open("me/summary.txt", "r", encoding="utf-8") as f:
+        with open("me/my_summary.txt", "r", encoding="utf-8") as f:
             self.summary = f.read()
 
 
@@ -116,7 +121,7 @@ If the user is engaging in discussion, try to steer them towards getting in touc
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            response = self.openai.chat.completions.create(model=self.model_name, messages=messages, tools=tools)
             if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
